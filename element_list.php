94d50a0424243073bc835a348b2f02e9af5d884f
Marc Torres <torres.marc@gmail.com>
2010-12-03 16:10:03 +0100

    added README note, and changed base HTML page

--- element_list.php
+++ element_list.php
@@ -1,27 +1,7 @@
-<fieldset>
-  <legend>SAU - SUPORT</legend>
-  <ul>
-    <li>Consulta tickets SAU educació i planificador - <a href="https://smanagerw1.xtec.cat:7003/sm/index.do">Service Manager</a></li>
-  </ul>
-</fieldset>
-<fieldset>
-  <legend>1x1</legend>
-  <ul>
-    <li>Informació general interessant - <a href="https://sites.google.com/site/2010educat1x1/informacio-util">CRP Selva II</a></li>
-  </ul>
-</fieldset>
-<fieldset>
-  <legend>Geco Vignette (WEB institucional)</legend>
-  <ul>
-    <li>Vista prèvia - <a href="http://geco.gencat.intranet/portal/site/Educacio/menuitem.75e1c94eb5dd9e184ed22010b0c0e1a0/?vgnextoid=c9775c7d4b267110VgnVCM1000008d0c1e0aRCRD&vgnextchannel=c9775c7d4b267110VgnVCM1000008d0c1e0aRCRD&vgnextfmt=default">Preview</a></li>
-    <li>Administració - <a href="http://geco.gencat.intranet/">Consola d'administració</a></li>
-  </ul>
-</fieldset>
-<fieldset>
-  <legend>Altres</legend>
-  <ul>
-    <li>Dades contacta centres - <a href="http://www10.gencat.net/pls/ense_ensenyam/p01.menu">Consulta centres</a></li>
-    <li>Intranet educació - <a href="http://educacio.gencat.cat/intranet">Intranet</a></li>
-  </ul>
-</fieldset>
+<p>
+  This code would be ditable when gaining access to the edit action (password protected)
+</p>
+<p>
+  Be aware that you could not give to anyone the password for editing as it's a XSS potential attack (only you could be able to edit your own code, not anyone!)
+</p>
 
